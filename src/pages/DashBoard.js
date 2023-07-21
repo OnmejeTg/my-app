8730469423eeb38723e7b380b2af6116782713bb
OnmejeTg <meettg2014@gmail.com>
2023-07-22 00:20:25 +0100

    dashboard

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,158 +1,167 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { Pie } from "react-chartjs-2";
+
 
 const DashBoard = () => {
-const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-
-    React.useEffect(() => {
-        fetch('http://127.0.0.1:8000/AdminUser/portal-analytics')
-          .then((response) => response.json())
-          .then((data) => setData(data))
-          .catch((error) => console.log(error));
-      }, [setData]);
+  React.useEffect(() => {
+    fetch("http://127.0.0.1:8000/AdminUser/portal-analytics")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error));
+  }, [setData]);
   return (
     <div className="container-fluid">
-
-    {/* <!-- Page Heading --> */}
-    <div className="d-sm-flex align-items-center justify-content-between mb-4">
+      {/* <!-- Page Heading --> */}
+      <div className="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
-    </div>
+      </div>
 
-    {/* <!-- Content Row --> */}
-    <div className="row">
-
+      {/* <!-- Content Row --> */}
+      <div className="row">
         {/* <!-- Earnings (Monthly) Card Example --> */}
         <div className="col-xl-3 col-md-6 mb-4">
-            <div className="card border-left-primary shadow h-100 py-2">
-                <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Termly Fees</div>
-                            <div className="h5 mb-0 font-weight-bold text-gray-800">₦{data.fees_paid}</div>
-                        </div>
-                        <div className="col-auto">
-                            <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
+          <div className="card border-left-primary shadow h-100 py-2">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                    Termly Fees
+                  </div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">
+                    ₦{data.fees_paid}
+                  </div>
                 </div>
+                <div className="col-auto">
+                  <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
 
         {/* <!-- Earnings (Monthly) Card Example --> */}
         <div className="col-xl-3 col-md-6 mb-4">
-            <div className="card border-left-success shadow h-100 py-2">
-                <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Fees Due</div>
-                            <div className="h5 mb-0 font-weight-bold text-gray-800">₦{data.fees_due}</div>
-                        </div>
-                        <div className="col-auto">
-                            <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
+          <div className="card border-left-success shadow h-100 py-2">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
+                    Fees Due
+                  </div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">
+                    ₦{data.fees_due}
+                  </div>
                 </div>
+                <div className="col-auto">
+                  <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
 
         {/* <!-- Earnings (Monthly) Card Example --> */}
         <div className="col-xl-3 col-md-6 mb-4">
-            <div className="card border-left-info shadow h-100 py-2">
-                <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Students
-                            </div>
-                            <div className="row no-gutters align-items-center">
-                                <div className="col-auto">
-                                    <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">{data.student}</div>
-                                </div>
-                                <div className="col">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-auto">
-                            <i className="fas fa-user-graduate fa-2x text-gray-300"></i>
-                        </div>
+          <div className="card border-left-info shadow h-100 py-2">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-info text-uppercase mb-1">
+                    Students
+                  </div>
+                  <div className="row no-gutters align-items-center">
+                    <div className="col-auto">
+                      <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                        {data.student}
+                      </div>
                     </div>
+                    <div className="col"></div>
+                  </div>
                 </div>
+                <div className="col-auto">
+                  <i className="fas fa-user-graduate fa-2x text-gray-300"></i>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
 
         {/* <!-- Pending Requests Card Example --> */}
         <div className="col-xl-3 col-md-6 mb-4">
-            <div className="card border-left-warning shadow h-100 py-2">
-                <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                 Staff</div>
-                            <div className="h5 mb-0 font-weight-bold text-gray-800">{data.staff}</div>
-                        </div>
-                        <div className="col-auto">
-                            <i className="fas fa-chalkboard-teacher fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
+          <div className="card border-left-warning shadow h-100 py-2">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                    Staff
+                  </div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">
+                    {data.staff}
+                  </div>
                 </div>
+                <div className="col-auto">
+                  <i className="fas fa-chalkboard-teacher fa-2x text-gray-300"></i>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
 
-    {/* <!-- Content Row --> */}
+      {/* <!-- Content Row --> */}
 
-    <div className="row">
-
+      <div className="row">
         {/* <!-- Area Chart --> */}
         <div className="col-xl-8 col-lg-7">
-            <div className="card shadow mb-4">
-                {/* <!-- Card Header - Dropdown --> */}
-                <div
-                    className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 className="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                </div>
-                {/* <!-- Card Body --> */}
-                <div className="card-body">
-                    <div className="chart-area">
-                        <canvas id="myAreaChart"></canvas>
-                    </div>
-                </div>
+          <div className="card shadow mb-4">
+            {/* <!-- Card Header - Dropdown --> */}
+            <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+              <h6 className="m-0 font-weight-bold text-primary">
+                Earnings Overview
+              </h6>
             </div>
+            {/* <!-- Card Body --> */}
+            <div className="card-body">
+              <div className="chart-area">
+                <canvas id="myAreaChart"></canvas>
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* <!-- Pie Chart --> */}
         <div className="col-xl-4 col-lg-5">
-            <div className="card shadow mb-4">
-                {/* <!-- Card Header - Dropdown --> */}
-                <div
-                    className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 className="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                    
-                </div>
-                {/* <!-- Card Body --> */}
-                <div className="card-body">
-                    <div className="chart-pie pt-4 pb-2">
-                        <canvas id="myPieChart"></canvas>
-                    </div>
-                    <div className="mt-4 text-center small">
-                        <span className="mr-2">
-                            <i className="fas fa-circle text-primary"></i> Direct
-                        </span>
-                        <span className="mr-2">
-                            <i className="fas fa-circle text-success"></i> Social
-                        </span>
-                        <span className="mr-2">
-                            <i className="fas fa-circle text-info"></i> Referral
-                        </span>
-                    </div>
-                </div>
+          <div className="card shadow mb-4">
+            {/* <!-- Card Header - Dropdown --> */}
+            <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+              <h6 className="m-0 font-weight-bold text-primary">
+                Revenue Sources
+              </h6>
             </div>
+            {/* <!-- Card Body --> */}
+            <div className="card-body">
+              <div className="chart-pie pt-4 pb-2">
+                <canvas id="myPieChart"></canvas>
+              </div>
+              <div className="mt-4 text-center small">
+                <span className="mr-2">
+                  <i className="fas fa-circle text-primary"></i> Direct
+                </span>
+                <span className="mr-2">
+                  <i className="fas fa-circle text-success"></i> Social
+                </span>
+                <span className="mr-2">
+                  <i className="fas fa-circle text-info"></i> Referral
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default DashBoard
+export default DashBoard;
